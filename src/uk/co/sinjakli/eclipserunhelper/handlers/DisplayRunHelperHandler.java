05e8aaeef73d67c13217bb18c3ce35b6ffabf133
Chris Sinjakli <chris@sinjakli.co.uk>
2012-08-08 20:49:18 +0100

    Show name of last launch in dialog (and other changes that I can't be arsed unpicking to make this commit atomic).

--- src/uk/co/sinjakli/eclipserunhelper/handlers/DisplayRunHelperHandler.java
+++ src/uk/co/sinjakli/eclipserunhelper/handlers/DisplayRunHelperHandler.java
@@ -1,9 +1,15 @@
 package uk.co.sinjakli.eclipserunhelper.handlers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.PopupDialog;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 
@@ -13,6 +19,12 @@ public class DisplayRunHelperHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
+		final ILaunch[] launchHistory = launchManager.getLaunches();
+		
+		final List<ILaunch> availableLaunches = new ArrayList<ILaunch>();
+		availableLaunches.addAll(Arrays.asList(launchHistory));
+		
 		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
 
 			@Override
@@ -20,9 +32,7 @@ public class DisplayRunHelperHandler extends AbstractHandler {
 				final Shell activeShell = PlatformUI.getWorkbench()
 						.getActiveWorkbenchWindow().getShell();
 
-				final RunHelperDialog dialog = new RunHelperDialog(activeShell,
-						PopupDialog.INFOPOPUPRESIZE_SHELLSTYLE, true, false,
-						false, false, false, null, null);
+				final RunHelperDialog dialog = new RunHelperDialog(activeShell, availableLaunches);
 
 				dialog.open();
 			}
--- src/uk/co/sinjakli/eclipserunhelper/ui/RunHelperDialog.java
+++ src/uk/co/sinjakli/eclipserunhelper/ui/RunHelperDialog.java
@@ -1,5 +1,8 @@
 package uk.co.sinjakli.eclipserunhelper.ui;
 
+import java.util.List;
+
+import org.eclipse.debug.core.ILaunch;
 import org.eclipse.jface.dialogs.PopupDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -11,20 +14,21 @@ import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 
 public class RunHelperDialog extends PopupDialog {
-
-	public RunHelperDialog(Shell parent, int shellStyle,
-			boolean takeFocusOnOpen, boolean persistSize,
-			boolean persistLocation, boolean showDialogMenu,
-			boolean showPersistActions, String titleText, String infoText) {
-		
-		super(parent, shellStyle, takeFocusOnOpen, persistSize, persistLocation,
-				showDialogMenu, showPersistActions, titleText, infoText);
-	}
 	
+	private final List<ILaunch> availableLaunches;
+
+	public RunHelperDialog(final Shell parent, final List<ILaunch> availableLaunches) {
+
+		super(parent, PopupDialog.INFOPOPUP_SHELLSTYLE, true, false,
+				false, false, false, null, null);
+		
+		this.availableLaunches = availableLaunches;
+	}
+
 	@Override
 	protected Control createDialogArea(Composite parent) {
 		Composite composite = (Composite) super.createDialogArea(parent);
-		
+
 		Table table = new Table(composite, SWT.SINGLE | SWT.BORDER);
 		table.setLinesVisible(true);
 		table.setHeaderVisible(false);
@@ -32,23 +36,23 @@ public class RunHelperDialog extends PopupDialog {
 		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
 		data.heightHint = 200;
 		table.setLayoutData(data);
-		
+
 		new TableColumn(table, SWT.NONE).setText("Col1");
 		new TableColumn(table, SWT.NONE).setText("Col2");
 		
-		TableItem line1 = new TableItem(table, SWT.NONE);
-		line1.setText(0, "Run previous Launch");
-		line1.setText(1, "1");
-		
-		TableItem line2 = new TableItem(table, SWT.NONE);
-		line2.setText(0, "Run 2nd previous Launch");
-		line2.setText(1, "2");
-		
+		int launchCounter = 1;
+		for (ILaunch launch : availableLaunches) {
+			TableItem item = new TableItem(table, SWT.NONE);
+			item.setText(0, launch.getLaunchConfiguration().getName());
+			item.setText(1, String.valueOf(launchCounter));
+			launchCounter++;
+		}
+
 		table.getColumn(0).pack();
 		table.getColumn(1).pack();
-		
+
 		composite.pack();
-		
+
 		return composite;
 	}
 
