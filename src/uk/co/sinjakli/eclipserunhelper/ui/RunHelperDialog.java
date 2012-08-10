7b62ecaee3f76923172c29c59b728cf5f17790be
Chris Sinjakli <chris@sinjakli.co.uk>
2012-08-10 22:15:01 +0100

    Add launch configuration type images.

--- src/uk/co/sinjakli/eclipserunhelper/ui/RunHelperDialog.java
+++ src/uk/co/sinjakli/eclipserunhelper/ui/RunHelperDialog.java
@@ -1,13 +1,17 @@
 package uk.co.sinjakli.eclipserunhelper.ui;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.jface.dialogs.PopupDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
@@ -19,6 +23,7 @@ import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -33,6 +38,7 @@ public class RunHelperDialog extends PopupDialog {
 
 	private final Map<ILaunchConfiguration, String> availableLaunches;
 	private final ILog logger;
+	private final Set<Image> disposableImages;
 
 	public RunHelperDialog(final Shell parent, final Map<ILaunchConfiguration, String> availableLaunches) {
 
@@ -41,6 +47,7 @@ public class RunHelperDialog extends PopupDialog {
 
 		this.availableLaunches = availableLaunches;
 		logger = RunHelperPlugin.getDefault().getLog();
+		disposableImages = new HashSet<Image>();
 	}
 
 	@Override
@@ -67,6 +74,21 @@ public class RunHelperDialog extends PopupDialog {
 			public String getText(final Object element) {
 				final ILaunchConfiguration launchConfiguration = (ILaunchConfiguration) element;
 				return "Run " + launchConfiguration.getName();
+			}
+
+			@Override
+			public Image getImage(final Object element) {
+				final ILaunchConfiguration launchConfiguration = (ILaunchConfiguration) element;
+				try {
+					final ILaunchConfigurationType launchConfigurationType = launchConfiguration.getType();
+					final Image launchImage = DebugUITools.getDefaultImageDescriptor(launchConfigurationType).createImage(true);
+					disposableImages.add(launchImage);
+					return launchImage;
+				} catch (final CoreException e) {
+					final IStatus errorStatus = RunHelperPlugin.errorStatus("Error getting launch configuration type.", e);
+					logger.log(errorStatus);
+				}
+				return super.getImage(element);
 			}
 		});
 
@@ -138,6 +160,14 @@ public class RunHelperDialog extends PopupDialog {
 			}
 		}
 
+	}
+
+	@Override
+	public boolean close() {
+		for (final Image image : disposableImages) {
+			image.dispose();
+		}
+		return super.close();
 	}
 
 }
